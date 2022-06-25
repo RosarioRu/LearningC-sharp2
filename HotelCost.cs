87171d361758987946b484b917bfd5f884af0404
RosarioRu <mariarruval@gmail.com>
2022-06-24 19:43:56 -0700

    Add method to uppercase inputs.

--- HotelCost.cs
+++ HotelCost.cs
@@ -5,12 +5,13 @@ class HotelCost
   static void Main()
   {
     Console.WriteLine("In what season are you booking a stay?");
-    string season = Console.ReadLine();
-
+    string season = Console.ReadLine().ToUpper();
+    Console.WriteLine("Can they be strung?: " + season);
+    
     Console.WriteLine("On the weekend or a weeknight?");
-    string dayOfWeek = Console.ReadLine();
-
-    bool summer = season == "summer" || "Summer";
-    bool weekend = dayOfWeek == "weekend" || "Weekend";
+    string dayOfWeek = Console.ReadLine().ToUpper();
+  
+    bool summer = season == "SUMMER";
+    bool weekend = dayOfWeek == "WEEKEND";
   }
 }
