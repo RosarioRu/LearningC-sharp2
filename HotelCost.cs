f5f9628f1aebe561f7f596888e26828516d99765
RosarioRu <mariarruval@gmail.com>
2022-06-24 19:52:28 -0700

    Add branching based on booleans

--- HotelCost.cs
+++ HotelCost.cs
@@ -13,5 +13,23 @@ class HotelCost
   
     bool summer = season == "SUMMER";
     bool weekend = dayOfWeek == "WEEKEND";
+
+    if (summer && weekend) //are true?
+    {
+      Console.WriteLine("Your stay is probably going to be pretty expensive. It's both peak travel season AND the weekend!");
+    }
+    else if (summer) //is true
+    {
+      Console.WriteLine("Your stay might be more expensive than normal due to it taking place during peak season!");
+    }
+    else if (weekend) //is true
+    {
+      Console.WriteLine("Your stay might be more expensive than normal due to it taking place during the weekend!");
+    }
+    else
+    {
+      Console.WriteLine("Your stay might be expensive, but it could be worse!");
+    }
   }
+
 }
